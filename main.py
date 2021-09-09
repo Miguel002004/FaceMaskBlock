26062e742fc3cefff27141fb07306a6306f37dcb
Miguel002004 <miguel002004@hotmail.com>
2021-09-09 10:41:43 -0500

    intento1

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import cv2
+import os
+import numpy as np
+dataPath = "./Dataset_faces"
+dir_list = os.listdir(dataPath)
+print("Lista archivos:", dir_list)
+labels = []
+facesData = []
+label = 0
+for name_dir in dir_list:
+    dir_path = dataPath + "/" + name_dir
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for file_name in os.listdir(dir_path):
+        image_path = dir_path + "/" + file_name
+        print(image_path)
+        image = cv2.imread(image_path, 0)
+        #cv2.imshow("Image", image)
+        #cv2.waitKey(10)
+        facesData.append(image)
+        labels.append(label)
+    label += 1
+print("Etiqueta 0: ", np.count_nonzero(np.array(labels) == 0))
+print("Etiqueta 1: ", np.count_nonzero(np.array(labels) == 1))
+# LBPH FaceRecognizer
+face_mask = cv2.face.LBPHFaceRecognizer_create()
+# Entrenamiento
+print("Entrenando...")
+face_mask.train(facesData, np.array(labels))
+# Almacenar modelo
+face_mask.write("face_mask_model.xml")
+print("Modelo almacenado")
