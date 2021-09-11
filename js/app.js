2a73a9777ec88d7d349f2839c9410fc54878d644
GenaroJavier <genaro120300@gmail.com>
2021-09-11 14:52:31 -0500

    Registro de fecha y hora funcional

---
+++ js/app.js
@@ -0,0 +1,41 @@
+var dates = new Date(); 
+var fecha = `${dates.getDay()}-${dates.getMonth()}-${dates.getFullYear()}`;
+var hora = `${dates.getHours()}:${dates.getMinutes()}`; 
+
+//var fecha =  new Date().toISOString().slice(0, 19).replace('T', ' '); 
+var mysql = require('mysql');
+var conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'facemaskblock', 
+    user: 'root',
+    password: ''
+}); 
+
+conexion.connect(function(error){
+    if(error) {
+        throw error; 
+    } else {
+        console.log("Conexion Exitosa"); 
+    }
+}); 
+
+//funcion  mostrar
+conexion.query('SELECT * FROM informacion', function (error, filas) {
+    if (error) {
+        throw error; 
+    } else {
+        filas.forEach(fila => {
+            console.log(fila); 
+        });
+    }
+}); 
+
+//funcion  insertar
+conexion.query(`INSERT INTO informacion (fecha, hora, cubrebocas) values ("${fecha}", "${hora}", 1)`, function (error, results) {
+    if (error)  throw error;
+    else {
+        console.log('Registro Agregado', results); 
+    }
+}); 
+
+conexion.end(); 
