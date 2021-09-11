1f5a7d1255193c4b4ae450bc3d7506a402fcdd14
Miguel002004 <miguel002004@hotmail.com>
2021-09-11 02:23:38 -0500

    se establecio comunicacion por sockets

--- js/Temperatura/app.js
+++ js/Temperatura/app.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var http = require('http');
+
 var app = express();
+
+const io = require('socket.io')(80);
+
 
 app.get("/",function(req,res) {
     res.send('<h1>hola</h1>');
   });
 
-  app.use(express.static('public'));
+app.use(express.static('public'));
+//========================================================================================
+io.on('connection', function (socket) {
+  console.log("Connected succesfully to the socket ...");
+
+  var news = [
+      { title: 'The cure of the Sadness is to play Videogames',date:'04.10.2016'},
+      { title: 'Batman saves Racoon City, the Joker is infected once again',date:'05.10.2016'},
+      { title: "Deadpool doesn't want to do a third part of the franchise",date:'05.10.2016'},
+      { title: 'Quicksilver demand Warner Bros. due to plagiarism with Speedy Gonzales',date:'04.10.2016'},
+  ];
+
+  // Enviar noticias al socket
+  socket.emit('news', news);
+
+  socket.on('my other event', function (data) {
+      console.log(data);
+  });
+});
+//==================================================================================================
 app.listen(8080);
